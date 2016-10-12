471963fe279c74bf1296e09b031c9d92c05032a3
Matthew D. Alexander <mda54092@gmail.com>
2016-10-12 00:11:53 -0500

    added webpage and finished markdown

--- course_project_notes.R
+++ course_project_notes.R
@@ -8,40 +8,43 @@ test <- read.csv("pml-testing.csv",  na.strings=c("NA","#DIV/0!",""))
 names(train)
 summary(train$classe)
 
-# the classe variable is the one to predict on 
 
-carlitos = subset(data, user_name == "carlitos")
-table(carlitos$classe)
-transform(carlitos, classe=factor(classe))
+library(caTools)
+set.seed(2000)
+
+split = sample.split(train$classe, SplitRatio = 0.6)
+train = subset(train, split == TRUE)
+validation = subset(train, split == FALSE)
 
 # data cleaning
 
 key_columns <- c(7:11, 37:49, 60:68, 84:86, 102, 113:124, 151:159, 160)
 train_small <- train[,key_columns]
+validation_small <- validation[,key_columns]
 test_small <- test[,key_columns]
-
-
-
-
-
-# svd 
-carlitos_matrix <- data.matrix(carlitos_small, rownames.force = NA)
-
-svd1 <- svd(carlitos_matrix)
-
-
-
-
 
 # tree
 
 tree <- rpart(classe ~ ., data=train_small, method="class")
-predictions_tree <- predict(tree, test_small, type = "class")
+predictions_tree <- predict(tree, validation_small, type = "class")
+
+length(validation_small$classe)
+length(predictions_tree)
+dim(validation_small)
+
+table(validation_small$classe, predictions_tree)
+confusionMatrix(validation_small$classe, predictions_tree)
+
 
 # random forest ( this classified the test best on the quiz)
 rf <- randomForest(classe ~ ., data=train_small, ntree = 200)
-predictions_rf <- predict(rf, test_small)
+predictions_rf <- predict(rf, validation_small)
 
 #we need a validation set to test on
-confusionMatrix(predictions_tree, test_small)
+confusionMatrix(predictions_tree, validation_small)
+
+
+predictions_rf_test <- predict(rf, test_small)
+
+
 
